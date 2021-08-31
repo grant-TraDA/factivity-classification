7df95b7c62c8735d63c88a58a14ffb38c56a0710
Seweryn Karolina <sewerynnkarolinaa@gmail.com>
2021-08-31 21:49:34 +0200

    Add important imports, modify data format

--- src/data.py
+++ src/data.py
@@ -2,16 +2,29 @@ import pandas as pd
 import torch
 
 from torch.utils.data import Dataset, DataLoader
+from transformers import AutoModel, AutoTokenizer
 
 
 class VeridicalDataset(Dataset):
-    def __init__(self, X, labels=None, labels2id=None, id2label=None):
+    def __init__(
+        self,
+        X,
+        labels=None,
+        label2id=None,
+        id2label=None,
+        text_col='text',
+        y_col='label',
+    ):
         self.X = X
+        self.text_col = text_col
+        self.y_col = y_col
         self.tokenizer = AutoTokenizer.from_pretrained(
-            "allegro/herbert-klej-cased-tokenizer-v1"
+            "allegro/herbert-klej-cased-tokenizer-v1",
+            max_len=512
         )
         if labels is None:
-            self.labels = sorted(list(set(X['label'])))
+            labels = [elem[y_col] for elem in X]
+            self.labels = sorted(list(set(labels)))
             self.label2id = {label: i for i, label in enumerate(self.labels)}
             self.id2label = {i: label for i, label in enumerate(self.labels)}
         else:
@@ -24,11 +37,11 @@ class VeridicalDataset(Dataset):
         return len(self.X)
     
     def __getitem__(self, index):
-        return self._preprocess(X[index])
+        return self._preprocess(self.X[index])
     
     def _preprocess(self, X):
-        encoding = self.tokenizer(X['text'], padding='max_length', truncation=False)
-        label = self.label2id(X['label'])
+        encoding = self.tokenizer(X[self.text_col], padding='max_length', truncation=True)
+        label = self.label2id[X[self.y_col]]
         return (
             torch.tensor(encoding['input_ids']),
             torch.tensor(encoding['attention_mask']),

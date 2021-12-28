128d7f557a79aa43033d66e6b20b9bf1517e9b6c
Seweryn Karolina <sewerynnkarolinaa@gmail.com>
2021-12-28 11:29:17 +0100

    Add model config

--- main_random_forest.py
+++ main_random_forest.py
@@ -5,17 +5,16 @@ from pathlib import Path
 from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
 from torch.utils.data import DataLoader
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 
 from src.data import VeridicalDataset
 from src.herbert_classfier import HerBERTClassifier
-from src.utils import plot_feature_importance
-
+from src.utils import plot_feature_importance, summarize_model_per_factive
 
 
 Y_COL = 'GOLD <T,H>'
 N_ESTIMATORS = 100
+MAX_DEPTH = 20
 
 DIR_PROJECT = Path(".").resolve()
 MODEL_DIR = DIR_PROJECT.joinpath("models")
@@ -30,36 +29,19 @@ dev = pd.read_csv(DIR_DATA.joinpath("dev_data.csv"))
 
 train = train.drop(['T PL', 'H PL', 'verb'], axis=1)
 test = test.drop(['T PL', 'H PL', 'verb'], axis=1)
-  
-
-#columns_model = [Y_COL, 'verb','verb - main semantic class','verb - tense','verb - factive/nonfactive','complement - tense','T - negation','T - type of sentence']
-
-# train = train[columns_model]
-# test = test[columns_model]
-    
-# for colname in ['verb', 'verb - main semantic class', 'verb - tense', 'verb - factive/nonfactive', 'complement - tense', 'T - type of sentence']:
-#     if colname in train.columns:
-#         train[colname] = train[colname].astype('category')
-#         test[colname] = test[colname].astype('category')
-
-#columns_to_dummify = [column for column in train.columns if ((column != Y_COL) and (column != "verb"))]
 
 X_train = train.drop(labels=[Y_COL], axis=1)
 
-#from sklearn.preprocessing import OrdinalEncoder
 # Train model
-classifier = RandomForestClassifier(random_state=10, n_estimators=N_ESTIMATORS)
-# classifier = Pipeline([
-# #    ('trans', OneHotEncoder(handle_unknown="ignore")),
-#     #('trans', OrdinalEncoder(handle_unknown= "use_encoded_value", unknown_value=-1)),
-#     ('model', RandomForestClassifier(random_state=10, n_estimators=N_ESTIMATORS))
-# ])
+classifier = RandomForestClassifier(
+    n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH,
+    class_weight={'C': 2, 'E': 1, 'N': 1}, random_state=123
+)
 classifier.fit(X_train, train[Y_COL])
 
-filename = MODEL_DIR.joinpath(f'rf_n_estim_{N_ESTIMATORS}.pkl')
+filename = MODEL_DIR.joinpath(f'rf_n_estim_{N_ESTIMATORS}_{MAX_DEPTH}.pkl')
 joblib.dump(classifier, filename)
 
-#feat = [item for sublist in classifier.named_steps['trans'].categories_ for item in sublist]
 plot_feature_importance(
     X_train.columns,
     classifier.feature_importances_,
@@ -70,11 +52,5 @@ plot_feature_importance(
 logging.info("\nTEST")
 y_pred = classifier.predict(test.drop(labels=[Y_COL], axis=1))
 logging.info(classification_report(test[Y_COL], y_pred, digits=4))
-
-# prediction on the entire dataset
-# df_data_path = DIR_DATA.joinpath("df.csv")
-# df = pd.read_csv(df_data_path)
-# y_pred = classifier.predict(df[list(X_train.columns)])
-# df['y_pred_rf'] = y_pred
-
-#df.to_csv(df_data_path, index=False)
+summary = summarize_model_per_factive(y_pred, test[Y_COL], test["verb - factive/nonfactive"])
+logging.info(summary)
